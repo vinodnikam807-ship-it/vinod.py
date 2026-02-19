d7009aab5b8fcb34464eee2c22c21f5381014469
vinodnikam807-ship-it <vinodnikam807@gmail.com>
2026-02-20 00:59:45 +0530

    Add files via upload

---
+++ pramod.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Welcome to Pramod Nikam")
+name = st.text_input ("Enter your name")
+sname = st.text_input("Enter your surname")
+email = st.text_input("Enter your email")
+phone = st.text_input("enter your phone number")
